cc332348f792820ae0a79e44a4241dad4974f476
unknown <you@example.com>
2022-12-03 08:05:18 +0530

    Added : Employee payroll data object

--- FormValidation.js
+++ FormValidation.js
@@ -3,3 +3,118 @@ let output = document.querySelector('.salary-output-text');
 salary.addEventListener('input', function() {
     output.textContent = salary.value;
 });
+
+class EmployeePayrollData {
+
+    constructor(...params) {
+        this.name = params[0];
+        this.profileImage = params[1];
+        this.gender = params[2];
+        this.department = params[3];
+        this.salary = params[4];
+        this.startDate = params[5];
+        this.notes = params[6];
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    set name(name) {
+        this._name = name;
+    }
+
+    get profileImage() {
+        return this._profileImage;
+    }
+
+    set profileImage(profileImage) {
+        this._profileImage = profileImage;
+    }
+
+    get gender() {
+        return this._gender;
+    }
+
+    set gender(gender) {
+        
+        this._gender = gender;
+        
+    }
+
+    get department() {
+        return this._department;
+    }
+
+    set department(department) {
+        this._department = department;
+    }
+
+    get salary() {
+        return this._salary;
+    }
+
+    set salary(salary) {
+        
+        this._salary = salary;
+    }
+
+    get startDate() {
+        return this._startDate;
+    }
+
+    set startDate(startDate) {
+        this._startDate = startDate;
+    }
+
+    get notes() {
+        return this._notes;
+    }
+
+    set notes(notes) {
+        this._notes = notes;
+    }
+
+    toString() {
+        return "Name = " + this.name + ", Profile Image = " + this.profileImage + ", Gender = " + this.gender + ", Department = " + this.department + ", Salary = " + this.salary + ", Start Date = " + employeeDate + ", Notes = " + this.notes;
+    }
+}
+
+function save() {
+
+    let employeeName = document.querySelector('#name').value;
+    let profileList = document.querySelectorAll('input[name="profile"]');
+    let employeeProfileImage;
+    for (let image of profileList) {
+        if (image.checked) {
+            employeeProfileImage = image.value;
+            break;
+        }
+    }
+
+    let genderList = document.querySelectorAll('input[name="gender"]');
+    let employeeGender;
+    for (let gender of genderList) {
+        if (gender.checked) {
+            employeeGender = gender.value;
+            break;
+        }
+    }
+
+    let departmentList = document.querySelectorAll('.checkbox:checked');
+    let employeeDepartment = new Array();
+    for (let department of departmentList) {
+        if (department.checked) {
+            employeeDepartment.push(department.value);
+        }
+    }
+
+    let employeeSalary = document.querySelector('#salary').value;
+
+    let day = document.querySelector('#day').value;
+    let month = document.querySelector('#month').value;
+    let year = document.querySelector('#year').value;
+    let employeeStartDate = new Date(year, month, day);
+
+    let employeeNotes = document.querySelector('#notes').value;
+}
